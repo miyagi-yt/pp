d289296a78b4949d568eb39aa39eea5a08f196b1
yoshiharu.takahashi.jp <yoshiharu.takahashi.jp@gmail.com>
2023-05-02 14:49:38 +0900

    feat: button

--- src/app/material/material-routing.module.ts
+++ src/app/material/material-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ButtonComponent } from './button/button.component';
 import { TreeComponent } from './tree/tree.component';
 
 const routes: Routes = [
+  { path: 'button', component: ButtonComponent },
   { path: 'tree', component: TreeComponent },
 ];
 @NgModule({
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTreeModule } from '@angular/material/tree';
 
 import { MaterialRoutingModule } from './material-routing.module';
+import { ButtonComponent } from './button/button.component';
 import { TreeComponent } from './tree/tree.component';
 
 
 @NgModule({
   declarations: [
-    TreeComponent
+    ButtonComponent,
+    TreeComponent,
   ],
   imports: [
     CommonModule,
 
     /** 追加 */
+    MatButtonModule,
     MatIconModule,
     MatTreeModule,
 
